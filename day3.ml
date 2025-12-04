1f3816804b87e6a90f3726601d427c36c23dac9d
Aaron Miller <apage43@ninjawhale.com>
2025-12-03 16:08:13 -0800

    day3p2

--- day3.ml
+++ day3.ml
@@ -21,3 +21,24 @@ let best_joltage_p1 bank =
 let () = List.map banks ~f:best_joltage_p1 |>
   List.fold ~init:0 ~f:(+) |>
   printf "p1 best joltage: %d\n"
+
+let activate_best (bank: int list) activate =
+  let banklen = List.length bank in
+  let front = List.take bank (banklen - (activate - 1)) in
+  let best = List.max_elt front ~compare:Int.ascending |> Option.value_exn in
+  let (i, _) = List.findi front ~f:(fun _i a -> a = best) |> Option.value_exn in
+  let rest = List.sub bank ~pos:(i + 1) ~len:(banklen - i - 1) in
+  (best, rest, activate - 1)
+
+let bp2 bank =
+  let rec loop acc bacc acts =
+    match activate_best bacc acts with
+    | (best, rest, acts) when acts > 0 -> loop (best :: acc) rest acts
+    | (best, _rest, _acts) -> best :: acc
+  in let acted = loop [] bank 12 in
+  List.map2_exn acted (List.init ~f:((+) 0) 12) ~f:(fun el p -> (10 ** p) * el) |>
+  List.fold ~init:0 ~f:(+)
+
+let () = List.map banks ~f:bp2 |>
+  List.fold ~init:0 ~f:(+) |>
+  printf "p2 best joltage: %d\n"

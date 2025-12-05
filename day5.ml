49b18cd238bcaf31c49d315bf07a607707c9606b
Aaron Miller <apage43@ninjawhale.com>
2025-12-05 11:34:01 -0800

    day5p2

--- day5.ml
+++ day5.ml
@@ -24,3 +24,28 @@ let in_any intervals el =
 let num_fresh_p1 = ingredients |> List.count ~f:(in_any fresh_ranges)
 
 let () = printf "p1 num fresh: %d\n" num_fresh_p1
+
+let merge_intervals (il: (int * int) list) =
+  let by_start = List.sort il ~compare:(Comparable.lift Int.compare ~f:(fun (s, _e) -> s)) in
+  match by_start with
+  | [] -> []
+  | hd :: tl -> 
+    let current = ref hd in
+    let rec loop merged ins =
+      match ins with
+      | [] -> !current :: merged
+      | (lo, hi) as next :: tl ->
+        let (clo, chi) = !current in
+        if (chi < lo) then
+          (let complete = !current in
+            current := next;
+            loop (complete :: merged) tl)
+        else
+            (current := (clo, max chi hi);
+            loop merged tl)
+        in loop [] tl
+
+let ilen_inclusive (lo, hi) = 1 + hi - lo
+let num_fresh_p2 = merge_intervals fresh_ranges |> List.sum (module Int) ~f:ilen_inclusive
+
+let () = printf "p2 num fresh: %d\n" num_fresh_p2

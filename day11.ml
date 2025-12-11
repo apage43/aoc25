38749e90294c0a8d6fd3899240d1d3fe2a9c509b
Aaron Miller <apage43@ninjawhale.com>
2025-12-10 22:02:04 -0800

    day11p2, counting

--- day11.ml
+++ day11.ml
@@ -29,3 +29,34 @@ let paths_from_to tbl from dest =
   !completed
 
 let () = printf "p1 answer: %d\n" (paths_from_to input "you" "out" |> List.length)
+
+let count_paths_from_to tbl from dest =
+  let memo = Hashtbl.create (module String) in
+  let rec count node visited =
+    if String.equal node dest then 1
+    else if Set.mem visited node then 0
+    else
+      match Hashtbl.find memo node with
+      | Some c -> c
+      | None ->
+          let visited' = Set.add visited node in
+          let outs = Hashtbl.find tbl node |> Option.value ~default:[] in
+          let total = List.fold outs ~init:0 ~f:(fun acc out ->
+            acc + count out visited') in
+          Hashtbl.set memo ~key:node ~data:total;
+          total
+  in
+  count from (Set.empty (module String))
+
+let count_paths_through tbl from node1 node2 dest =
+  let c1 = count_paths_from_to tbl from node1 in
+  let c2 = count_paths_from_to tbl node1 node2 in
+  let c3 = count_paths_from_to tbl node2 dest in
+  let c1' = count_paths_from_to tbl from node2 in
+  let c2' = count_paths_from_to tbl node2 node1 in
+  let c3' = count_paths_from_to tbl node1 dest in
+  (c1 * c2 * c3) + (c1' * c2' * c3')
+
+
+let () = 
+  printf "p2 answer: %d\n" (count_paths_through input "svr" "fft" "dac" "out")

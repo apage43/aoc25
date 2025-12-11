bef13548e613f297abd69bad0f6c41243b93d881
Aaron Miller <apage43@ninjawhale.com>
2025-12-10 21:59:35 -0800

    day11 inputs

--- day11.ml
+++ day11.ml
@@ -4,18 +4,20 @@ open Stdio
 let argv = Sys.get_argv ()
 let infilename = argv.(1)
 
-let input = In_channel.read_lines infilename |>
+let parse file = In_channel.read_lines file |>
   List.map ~f:(fun line ->
     match String.split ~on:':' line with
     | [dev; outs_s] -> (dev, String.split ~on:' ' outs_s |> List.tl_exn)
     | _ -> invalid_arg "bad parse")
   |> Hashtbl.of_alist_exn (module String)
 
-let paths_from_to from dest =
+let input = parse infilename
+
+let paths_from_to tbl from dest =
   let completed = ref [] in
   let rec search path =
       let cur = List.hd_exn path in
-      let outs = Hashtbl.find input cur
+      let outs = Hashtbl.find tbl cur
       |> Option.value ~default:[]
       |> List.filter ~f:(Fn.compose not (List.mem path ~equal:String.equal)) in
       List.iter outs ~f:(fun out ->
@@ -26,4 +28,4 @@ let paths_from_to from dest =
   let () = search [from] in
   !completed
 
-let () = printf "p1 answer: %d\n" (paths_from_to "you" "out" |> List.length)
+let () = printf "p1 answer: %d\n" (paths_from_to input "you" "out" |> List.length)
